87a2bbade660ce7837f46012c547e317229c8320
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-21 20:20:10 -0400

    find and update data in Store database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1);
+@store2 = Store.find_by(id: 2);
+
+pp @store1
+pp @store2
+
+@store1.name = 'new name'
+@store1.save
+
+pp @store1

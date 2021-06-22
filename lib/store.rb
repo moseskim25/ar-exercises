46e52abbc205fc55875ae27aa553ae0470a50bdb
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-21 23:01:34 -0400

    validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
 
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def carries_mens_or_womens_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, 'either mens or womens apparel must be present')
+      errors.add(:womens_apparel, 'either mens or womens apparel must be present')
+    end
+  end
 end
